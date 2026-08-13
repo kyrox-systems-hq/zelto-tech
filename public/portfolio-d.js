a5e7bf48fbc2db12ea45b650754cc7d15b3eb6e6
kyrox-systems-hq <102359647+kyrox-systems-hq@users.noreply.github.com>
2026-08-14 00:56:10 +0500

    Prioritise paid featured work

--- public/portfolio-d.js
+++ public/portfolio-d.js
@@ -1,1 +1,7 @@
-const {w,g,f,m,t,q,d,s}=ZP,tr=w.querySelector('.portfolio-shelf-track');w.querySelector('.portfolio-feature-primary').append(f);w.querySelector('.portfolio-feature-secondary').append(m,t);[q,...d].forEach(x=>{x.classList.add('portfolio-shelf-item');tr.append(x)});g.hidden=true;const p=s.querySelector('.proof-strip');if(p)w.querySelector('.portfolio-featured-grid').after(p);import('./portfolio-e.js').then(()=>import('./portfolio-f.js'));
+const {w,g,f,m,t,q,d,s}=ZP,tr=w.querySelector('.portfolio-shelf-track');
+w.querySelector('.featured-one').append(m);
+w.querySelector('.featured-two').append(t);
+[q,f,...d].forEach(x=>{x.classList.add('portfolio-shelf-item');tr.append(x)});
+g.hidden=true;
+const p=s.querySelector('.proof-strip');if(p)w.querySelector('.portfolio-featured-grid').after(p);
+import('./portfolio-e.js').then(()=>import('./portfolio-f.js'));
