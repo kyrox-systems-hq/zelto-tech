a7dd4e6020acb4aba93bb91cdd66c8992818b805
kyrox-systems-hq <102359647+kyrox-systems-hq@users.noreply.github.com>
2026-08-13 22:47:55 +0500

    Wire project shelf navigation

---
+++ public/portfolio-f.js
@@ -0,0 +1,1 @@
+const tr=ZP.w.querySelector('.portfolio-shelf-track'),step=()=>tr.firstElementChild?.getBoundingClientRect().width+18||tr.clientWidth;ZP.w.querySelector('[data-prev]').onclick=()=>tr.scrollLeft-=step();ZP.w.querySelector('[data-next]').onclick=()=>tr.scrollLeft+=step();
