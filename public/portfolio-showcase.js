983b15a5a55635de3d6ae0a86e6ab4c9d2b89a2e
kyrox-systems-hq <102359647+kyrox-systems-hq@users.noreply.github.com>
2026-08-14 01:55:10 +0500

    Use corrected homepage proof module

--- public/portfolio-showcase.js
+++ public/portfolio-showcase.js
@@ -1,1 +1,1 @@
-import('./portfolio-a.js').then(()=>import('./portfolio-b.js')).then(()=>import('./portfolio-c.js')).then(()=>import('./portfolio-d.js')).then(()=>import('./homepage-proof.js')).catch(console.error);
+import('./portfolio-a.js').then(()=>import('./portfolio-b.js')).then(()=>import('./portfolio-c.js')).then(()=>import('./portfolio-d.js')).then(()=>import('./homepage-proof-corrected.js')).catch(console.error);
